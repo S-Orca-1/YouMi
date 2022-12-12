39571811065f13a648cad4d8289489bfff6bb7df
Ravshan <ravshangiyosov2002@gmail.com>
2022-12-12 17:33:06 +0500

    psycholigist

--- apps/psychologist/models.py
+++ apps/psychologist/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Education(models.Model):
+    university = models.CharField(max_length=600)
+    faculty = models.CharField(max_length=600)
+    specialty = models.CharField(max_length=600)
+    degree = models.CharField(max_length=500)
+    start = models.DateField()
+    finish = models.DateField()
+    diplom = models.FileField()
+
+    def __str__(self):
+        return self.university
--- apps/rate/views.py
+++ apps/rate/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import RateSerializer
+from .models import Rate
 
-# Create your views here.
+
+class RateAPIView(generics.ListAPIView):
+    def get_queryset(self):
+        return Rate.objects.all()
+
+    def get_serializer_class(self):
+        return RateSerializer
